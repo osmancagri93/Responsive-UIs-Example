3a6ad6f7d38bf0a425f90cad08b067c33b04453f
osmancagri93 <72887944+osmancagri93@users.noreply.github.com>
2022-11-10 15:54:54 +0300

    Add files via upload

---
+++ lib/Architecture.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Architecture extends StatefulWidget {
+  const Architecture({Key? key}) : super(key: key);
+
+  @override
+  State<Architecture> createState() => _ArchitectureState();
+}
+
+class _ArchitectureState extends State<Architecture> {
+  @override
+  Widget build(BuildContext context) {
+    final boyut = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: boyut.width < 600
+          ? Drawer(
+              child: Menu(),
+            )
+          : null,
+      body: Row(
+        children: [
+          boyut.width > 600 ? Flexible(flex: 1, child: Menu()) : Container(),
+          Flexible(
+            flex: 3,
+            child: Center(
+              child: FittedBox(
+                child: Text(
+                  'Mimari Çizim Yapabileceğiniz Alan Boyutu: ${boyut.width.floor()}*${boyut.height.floor() - 150}',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Container(
+          height: 150.0,
+          color: Colors.green,
+          child: Center(child: Text("Mimari Çizim")),
+        ),
+        ListTile(
+          title: Text("Ana Sayfa"),
+          onTap: () {},
+        ),
+        ListTile(
+          title: Text("Profil"),
+          onTap: () {},
+        ),
+        ListTile(
+          title: Text("Araçlar"),
+          onTap: () {},
+        ),
+        ListTile(
+          title: Text("Kaydet"),
+          onTap: () {},
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_uis/Architecture.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Architecture(),
+  ));
+}
